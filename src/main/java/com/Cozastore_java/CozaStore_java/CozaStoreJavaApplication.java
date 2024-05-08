90f2bfaa2dafd442cc7a5dacbe03328b5c0065cd
quocdatn <quocdatnqd1996@gmail.com>
2024-05-08 16:12:22 +0700

    add

--- src/main/java/com/Cozastore_java/CozaStore_java/CozaStoreJavaApplication.java
+++ src/main/java/com/Cozastore_java/CozaStore_java/CozaStoreJavaApplication.java
@@ -2,8 +2,10 @@ package com.Cozastore_java.CozaStore_java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class CozaStoreJavaApplication {
 
 	public static void main(String[] args) {
